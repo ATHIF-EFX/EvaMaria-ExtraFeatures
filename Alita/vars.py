b388d20a2a0ec9d6709d912f3ceb6925174de354
Aadhi <87893573+Aadhi000@users.noreply.github.com>
2022-03-01 19:01:34 +0530

    Update vars.py

--- Alita/vars.py
+++ Alita/vars.py
@@ -1,8 +1,5 @@
 from os import getcwd
 
-from prettyconf import Configuration
-from prettyconf.loaders import EnvFile, Environment
-
 env_file = f"{getcwd()}/.env"
 config = Configuration(loaders=[Environment(), EnvFile(filename=env_file)])
 
